b0ba1a7a98bc239e4dcf22d32a8fd764e7494d59
kellykelly7 <kellytruong519@gmail.com>
2025-01-25 12:17:12 -0500

    changed the alert method for the button
    instead of console.log(), used alert.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("my-button");
 
 btn.addEventListener("click", () => {
 // adds pop-up window when event "click" occurs on button in html
-//  alert("You clicked me!");
+    alert("You clicked me!");
 // prints text to console instead of the alert pop-up
-    console.log("You clicked me!");
+//    console.log("You clicked me!");
 });
