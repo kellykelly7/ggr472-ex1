509003c8dfa89e2db4f5af6d3bf366877605228d
kellykelly7 <kellytruong519@gmail.com>
2025-01-25 11:40:24 -0500

    meant to practice publishing
    step 2 of exercise 2

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("my-button");
+
+btn.addEventListener("click", () => {
+// adds pop-up window when event "click" occurs on button in html
+//  alert("You clicked me!");
+// prints text to console instead of the alert pop-up
+    console.log("You clicked me!");
+});
